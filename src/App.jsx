4eaeb2cabb8d477f9fd3b10daeb05f57426d8ad1
Gabriel Saraiva <gabrielherera14@gmail.com>
2024-07-28 13:38:47 -0300

    feature: modal edit task

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import { PlusIcon, PencilSquareIcon, CheckBadgeIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { PlusIcon, PencilSquareIcon, CheckBadgeIcon, TrashIcon, XMarkIcon, CheckIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
+
 
 function App() {
   const [task, setTask] = useState('')
   const [description, setDescription] = useState('')
   const [itemsList, setItemsList] = useState([])
+  const [editTaskModal, setEditTaskModal] = useState(false)
+
   function changeInputTask(event) {
     const taskValue = event.target.value
     setTask(taskValue)
   }
+
   function changeInputDescription(event) {
     const descriptionValue = event.target.value
     setDescription(descriptionValue)
   }
+
   function addItem() {
     if (!task) {
       return (
@@ -23,18 +28,29 @@ function App() {
     setTask('')
     setDescription('')
   }
+
   function markAsComplete(id) {
     const checkedItems = itemsList.map((item) =>
       item.id === id ? { ...item, isCompleted: !item.isCompleted } : item
     );
     setItemsList(checkedItems);
   }
+
   function deleteItem(id) {
     const deletedItems = itemsList.filter((item) =>
       item.id !== id ? item : null
     );
     setItemsList(deletedItems);
   }
+
+  function openTaskModal() {
+    setEditTaskModal(true)
+  }
+
+  function closeTaskModal() {
+    setEditTaskModal(false)
+  }
+
   return (
 
     <div className="md:container md:mx-auto flex justify-center items-center h-screen bg-pattern bg-no-repeat bg-center">
@@ -54,7 +70,8 @@ function App() {
               name='tarefa'
               onChange={changeInputTask}
               value={task}
-              className="bg-transparent text-lg placeholder-zinc-400 outline-none border-none focus:ring-0 focus:border-transparent focus:placeholder-zinc-700 w-full" />
+              placeholder="Tarefa"
+              className="bg-transparent text-lg placeholder-zinc-600 outline-none border-none focus:ring-0 focus:border-transparent focus:placeholder-zinc-800 w-full" />
           </div>
 
           <p className="text-sm text-zinc-400 flex items-center"> Descrição da tarefa</p>
@@ -64,11 +81,49 @@ function App() {
               name='descricao'
               onChange={changeInputDescription}
               value={description}
-              size={100000}
-              className="bg-zinc-900 text-lg placeholder-zinc-400 outline-none border-none focus:ring-0 focus:border-transparent focus:placeholder-zinc-700 w-full" />
+              placeholder="Descrição da tarefa"
+              className="bg-zinc-900 text-lg placeholder-zinc-600 outline-none border-none focus:ring-0 focus:border-transparent focus:placeholder-zinc-800 w-full" />
           </div>
 
         </div>
+
+
+
+
+
+        {editTaskModal && (
+          <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
+            <div className='w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5'>
+              <div className='space-y-2'>
+                <div className='flex items-center justify-between'>
+                  <h2 className='text-lg font-semibold'>Editar Tarefa</h2>
+                  <button onClick={closeTaskModal} type='button' className="text-zinc-200 hover:text-zinc-400 transition-all">
+                    <XMarkIcon className="size-7" />
+                  </button>
+                </div>
+              </div>
+              <div className='space-y-3'>
+                <div className='h-14 px-0.5 py-2 bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2'>
+                  <input
+                    type="text"
+                    name='name'
+                    placeholder="Tarefa"
+                    className="bg-transparent text-lg placeholder-zinc-400 outline-none border-none focus:ring-0 focus:border-transparent focus:placeholder-zinc-700 flex-1" />
+                </div>
+                <div className='h-22 px-0.5 py-2 bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2'>
+                  <textarea
+                    type="text"
+                    name='descricao'
+                    placeholder="Descrição"
+                    className="bg-zinc-950 text-lg placeholder-zinc-400 outline-none border-none focus:ring-0 focus:border-transparent focus:placeholder-zinc-700 w-full" />
+                </div>
+                <button onClick={closeTaskModal} className="bg-teal-500 text-teal-950 hover:bg-teal-600 rounded-lg px-5 py-2 flex items-center justify-center gap-2 transition-all w-full">
+                  <CheckIcon className="size-5" /> Confirmar
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
         <div className="flex items-center justify-between w-full">
           <ul className="text-md w-full space-y-2">
             {itemsList.map(
@@ -76,17 +131,17 @@ function App() {
                 <>
                   <div className="hidden">{item.id = index}</div>
                   <div key={item.id} className={item.isCompleted ? "flex w-[600px] bg-lime-950 py-2.5 px-4 rounded-lg items-center gap-3 text-lime-900" : "flex w-[600px] bg-zinc-900 py-2.5 px-4 rounded-lg items-center gap-3"}>
-                    <button onClick={() => markAsComplete(item.id)}>
+                    <button title="Concluir tarefa" onClick={() => markAsComplete(item.id)}>
                       <CheckBadgeIcon className={item.isCompleted ? "size-7 text-lime-400 transition-all" : "text-zinc-400 size-7 hover:text-zinc-50 transition-all"} />
                     </button>
                     <li className="flex-1">
                       <p>{item.task}</p>
                       <p className={item.isCompleted ? "text-xs text-lime-900 text-wrap max-w-full" : "text-xs text-zinc-400 text-wrap max-w-full"}>{item.description}</p>
                     </li>
-                    <button>
-                      <PencilSquareIcon className={item.isCompleted ? "size-7 hover:text-zinc-500 transition-all" : "text-zinc-400 size-7 hover:text-zinc-50 transition-all"} />
+                    <button onClick={openTaskModal}>
+                      <PencilSquareIcon title="Editar tarefa" className={item.isCompleted ? "size-7 hover:text-zinc-500 transition-all" : "text-zinc-400 size-7 hover:text-zinc-50 transition-all"} />
                     </button>
-                    <button onClick={() => deleteItem(item.id)}>
+                    <button title="Excluir tarefa" onClick={() => deleteItem(item.id)}>
                       <TrashIcon className={item.isCompleted ? "size-7 hover:text-zinc-500 transition-all" : "text-zinc-400 size-7 hover:text-zinc-50 transition-all"} />
                     </button>
                   </div>
